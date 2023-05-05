cfd74fb5a3afd1bf4b3d7a9a7e67b91113c63e37
Asif Amin <asifaminisonline@gmail.com>
2023-05-05 10:35:40 +0530

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  # has_many :group_purchases, dependent: :destroy
-  # has_many :purchases, through: :group_purchases
   has_and_belongs_to_many :purchases, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2, maximum: 30 }
